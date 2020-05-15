d21840af8bcb567ad51a84b33d0be4e042367bd7
Jean-Michel <jm.fraullodra@gmail.com>
2020-05-15 14:51:27 +0200

    ADD 2 entities and main function

---
+++ src/entities/Book.kt
@@ -0,0 +1,5 @@
+package entities
+
+class Book constructor(val id: Int, val author: String, val title: String, val genre: String){
+
+}
---
+++ src/entities/Library.kt
@@ -0,0 +1,4 @@
+package entities
+
+class Library(val bookList: List<Book>) {
+}
